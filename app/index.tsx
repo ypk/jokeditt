ac69d32f914e194c9ee1aa4392940dc357cee765
ypk <yprasannakumar@gmail.com>
2024-06-20 01:09:56 +0100

    added libraries needed for the app updated app, babel and tailwind config modified app index page to read and display messages

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
 import { Text, View } from "react-native";
+import Constants from 'expo-constants';
 
 export default function Index() {
+  const appName = Constants.expoConfig.name;
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="font-bold">Welcome to {appName}</Text>
     </View>
   );
 }
